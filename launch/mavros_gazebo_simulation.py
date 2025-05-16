6bc3132e3bbbbf75c090aa48acd3a350cf8ddb6c
stibipet <stibipet@fel.cvut.cz>
2025-05-16 15:08:18 +0200

    modify mavros launch file to enable automated spawning

--- launch/mavros_gazebo_simulation.py
+++ launch/mavros_gazebo_simulation.py
@@ -4,7 +4,7 @@ import launch
 import os
 
 from launch_ros.actions import Node, PushROSNamespace, SetParameter
-from launch.actions import GroupAction, IncludeLaunchDescription, DeclareLaunchArgument
+from launch.actions import GroupAction, IncludeLaunchDescription, DeclareLaunchArgument, OpaqueFunction
 from launch.substitutions import (
     LaunchConfiguration,
     IfElseSubstitution,
@@ -17,15 +17,66 @@ from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 
 
+def process_launch_args(context, *args, **kwargs):
+    pkg_name = "mrs_uav_px4_api"
+    this_pkg_path = get_package_share_directory(pkg_name)
+
+    id_arg = LaunchConfiguration('ID').perform(context)
+    vehicle_name = LaunchConfiguration('VEHICLE_NAME').perform(context)
+    fcu_url = LaunchConfiguration('fcu_url').perform(context)
+    gcs_url = LaunchConfiguration('gcs_url').perform(context)
+
+
+    tgt_system = int(id_arg) + 1
+    uav_name = f'{vehicle_name}{id_arg}'
+    namespace = uav_name
+
+    # args from ENV
+    # use_sim_time=os.getenv('USE_SIM_TIME', "false") == "true"
+    respawn_mavros=os.getenv('respawn_mavros', "false") == "true"
+
+
+    px4_launch_arguments = {
+        "fcu_url": fcu_url,
+        "gcs_url": gcs_url,
+        "tgt_system": str(tgt_system),
+        "tgt_component": str(1),
+        "log_output": "screen",
+        "fcu_protocol": "v2.0",
+        "respawn_mavros": str(respawn_mavros),
+        "namespace": "mavros",
+        "pluginlists_yaml":  this_pkg_path + "/config/mavros_plugins.yaml",
+        "config_yaml": this_pkg_path + "/config/mavros_px4_config.yaml",
+    }
+
+    print(px4_launch_arguments.items())
+
+    # for some undiscovered reason, a python launch files crashes for mavros
+    # so we use this monstrosity to start a classic launch file from a python launch file
+    launch_xml_include_with_namespace = GroupAction(
+        actions=[
+            # push_ros_namespace first to set namespace of included nodes for following actions
+            PushROSNamespace(namespace),
+            IncludeLaunchDescription(
+                XMLLaunchDescriptionSource(
+                    os.path.join(
+                        get_package_share_directory('mavros'),
+                        'launch/node.launch')
+                    ),
+                    launch_arguments=px4_launch_arguments.items()
+            ),
+        ],
+    )
+
+    return [launch_xml_include_with_namespace]
+
+
+
 def generate_launch_description():
-    
+
     # launch.logging.get_logger().setLevel(launch.logging.logging.DEBUG)
 
     ld = launch.LaunchDescription()
-
-    pkg_name = "mrs_uav_px4_api"
-
-    this_pkg_path = get_package_share_directory(pkg_name)
 
     # #{ custom_config
 
@@ -52,99 +103,33 @@ def generate_launch_description():
 
     # #} end of custom_config
 
-    # #{ args from ENV
-
-    ID = os.getenv("ID", 1)
-    VEHICLE_NAME = os.getenv("VEHICLE_NAME", "uav")
-    fcu_url = os.getenv("fcu_url", f"udp://:1454{ID}@127.0.0.1:1458{ID}")
-    gcs_url = os.getenv("gcs_url", f"udp://:1455{ID}@127.0.0.1:1459{ID}")
-    use_sim_time=os.getenv('USE_SIM_TIME', "false") == "true"
-    respawn_mavros=os.getenv('respawn_mavros', "false") == "true"
-
-    # #} end of args from ENV
-
-    # the first one has the priority
-    # configs = [
-    #     this_pkg_path + '/config/mavros_px4_config.yaml',
-    #     get_package_share_directory("mavros") + "/launch/px4_config.yaml",
-    # ]
-
-    tgt_system = int(ID) + 1
-    uav_name = f"{VEHICLE_NAME}{ID}"
-    namespace = uav_name
-
-
-    px4_launch_arguments = {
-        "fcu_url": fcu_url,
-        "gcs_url": gcs_url,
-        "tgt_system": str(tgt_system),
-        "tgt_component": str(1),
-        "log_output": "screen",
-        "fcu_protocol": "v2.0",
-        "respawn_mavros": str(respawn_mavros),
-        "namespace": "mavros",
-        "pluginlists_yaml":  this_pkg_path + "/config/mavros_plugins.yaml",
-        "config_yaml": this_pkg_path + "/config/mavros_px4_config.yaml",
-    }
-
-    print(px4_launch_arguments.items())
+    # #{ args from mrs_drone_spawner
+    ld.add_action(DeclareLaunchArgument(
+        'ID',
+        default_value="1",
+        description='ID number assigned to the vehicle by the drone spawner'
+    ))
+    ld.add_action(DeclareLaunchArgument(
+        'VEHICLE_NAME',
+        default_value="uav",
+        description='prefix for the vehicle name, full name will be {VEHICLE_NAME}{ID}'
+    ))
+    ld.add_action(DeclareLaunchArgument(
+        'fcu_url',
+        default_value=f"udp://:14541@127.0.0.1:14581",
+        description='port setup for the fcu'
+    ))
+    ld.add_action(DeclareLaunchArgument(
+        'gcs_url',
+        default_value=f"udp://:14551@127.0.0.1:14591",
+        description='port setup for the gcs'
+    ))
+    # #}
 
 
-    launch_xml_include_with_namespace = GroupAction(
-        actions=[
-            # push_ros_namespace first to set namespace of included nodes for following actions
-            PushROSNamespace(uav_name),
-            IncludeLaunchDescription(
-                XMLLaunchDescriptionSource(
-                    os.path.join(
-                        get_package_share_directory('mavros'),
-                        'launch/node.launch')
-                    ),
-                    launch_arguments=px4_launch_arguments.items()
-            ),
-        ],
-        # parameters=[
-        #     {"fcu_url": fcu_url},
-        #     {"gcs_url": gcs_url},
-        #     {"tgt_system": tgt_system},
-        #     {"tgt_component": 1},
-        #     {"log_output": "screen"},
-        #     {"fcu_protocol": "v2.0"},
-        #     {"respawn_mavros": respawn_mavros},
-        #     {"namespace": f"{namespace}/mavros"},
-        #     {"pluginlists_yaml": get_package_share_directory("mavros") + "/launch/px4_pluginlists.yaml"},
-        #     {"config_yaml": configs},
-        # ],
-    )
-
-    ld.add_action(launch_xml_include_with_namespace)
-
-    # ld.add_action(
-        
-    #     Node(
-    #         namespace=namespace,
-    #         name='mavros',
-    #         package='mavros',
-    #         executable='mavros_node',
-        
-    #         parameters=[
-    #             {"fcu_url": fcu_url},
-    #             {"gcs_url": gcs_url},
-    #             {"tgt_system": tgt_system},
-    #             {"tgt_component": 1},
-    #             {"log_output": "screen"},
-    #             {"fcu_protocol": "v2.0"},
-    #             {"respawn_mavros": respawn_mavros},
-    #             {"namespace": f"{namespace}/mavros"},
-    #             {"pluginlists_yaml": get_package_share_directory("mavros") + "/launch/px4_pluginlists.yaml"},
-    #             {"config_yaml": configs},
-    #        ],
-
-    #        remappings=[
-    #            ("/diagnostics", f"/uav{ID}/diagnostics")
-    #         ],
-    #     )
-    # )
+    # this needs to be done whenever we want to parse and modify launch arguments
+    # directly in the launch file
+    ld.add_action(OpaqueFunction(function=process_launch_args))
 
     return ld
 
