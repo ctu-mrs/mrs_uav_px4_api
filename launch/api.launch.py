fb5652ef8803e08171d54b4903fd9342e323a909
Filip <filipmymail@gmail.com>
2026-04-11 13:51:14 +0200

    Update PX4 params and clean launch files

--- launch/api.launch.py
+++ launch/api.launch.py
@@ -7,7 +7,7 @@ from launch_ros.actions import ComposableNodeContainer, Node
 from launch_ros.descriptions import ComposableNode
 from launch.actions import DeclareLaunchArgument
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.actions import IncludeLaunchDescription, GroupAction, SetEnvironmentVariable
+from launch.actions import IncludeLaunchDescription
 from launch.conditions import IfCondition, UnlessCondition
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import (
@@ -24,7 +24,7 @@ def generate_launch_description():
 
     ld = launch.LaunchDescription()
 
-    pkg_name = "mrs_uav_px4_api"
+    pkg_name = 'mrs_uav_px4_api'
 
     this_pkg_path = get_package_share_directory(pkg_name)
 
@@ -37,8 +37,8 @@ def generate_launch_description():
     # default_value is required to if the arg is supposed to be optional at launch time
     ld.add_action(DeclareLaunchArgument(
         'custom_config',
-        default_value="",
-        description="Path to the custom configuration file. The path can be absolute, starting with '/' or relative to the current working directory",
+        default_value='',
+        description='Path to the custom configuration file. The path can be absolute, starting with "/" or relative to the current working directory',
     ))
 
     # behaviour:
@@ -59,7 +59,7 @@ def generate_launch_description():
 
     declare_simulation = DeclareLaunchArgument(
         'simulation',
-        default_value="true" if os.getenv('RUN_TYPE', "simulation") == "simulation" else "false",
+        default_value='true' if os.getenv('RUN_TYPE', 'simulation') == 'simulation' else 'false',
         description='Whether to start a as a simulation or load into an existing container.'
     )
 
@@ -69,21 +69,21 @@ def generate_launch_description():
 
     # #{ args from ENV
 
-    uav_name=os.getenv('UAV_NAME', "uav1")
-    use_sim_time=os.getenv('USE_SIM_TIME', "false") == "true"
-    old_px4_fw=os.getenv('OLD_PX4_FW', "false") == "true"
+    uav_name = os.getenv('UAV_NAME', 'uav1')
+    use_sim_time = os.getenv('USE_SIM_TIME', 'false') == 'true'
+    old_px4_fw = os.getenv('OLD_PX4_FW', 'false') == 'true'
 
     # #} end of args from ENV
 
     if old_px4_fw:
-        mavros_odometry_in_topic = "mavros/local_position/odom"
+        mavros_odometry_in_topic = 'mavros/local_position/odom'
     else:
-        mavros_odometry_in_topic = "mavros/odometry/in"
+        mavros_odometry_in_topic = 'mavros/odometry/in'
 
     # the first one has the priority
     configs = [
         this_pkg_path + '/config/px4_api.yaml',
-        get_package_share_directory("mrs_uav_hw_api") + "/config/hw_api.yaml",
+        get_package_share_directory('mrs_uav_hw_api') + '/config/hw_api.yaml',
     ]
 
     namespace = uav_name
@@ -94,48 +94,48 @@ def generate_launch_description():
         name=namespace+'_container',
         package='rclcpp_components',
         executable='component_container_mt',
-        output="screen",
+        output='screen',
 
         composable_node_descriptions=[
 
             ComposableNode(
 
-                package="mrs_uav_hw_api",
-                plugin="mrs_uav_hw_api::HwApiManager",
+                package='mrs_uav_hw_api',
+                plugin='mrs_uav_hw_api::HwApiManager',
                 namespace=uav_name,
-                name="hw_api",
+                name='hw_api',
                 parameters=[
 
-                    {"uav_name": uav_name},
-                    {"topic_prefix": "/" + uav_name},
-                    {"use_sim_time": use_sim_time},
-                    {"configs": configs},
-                    {"custom_config": custom_config},
-                    {"simulation": simulation},
+                    {'uav_name': uav_name},
+                    {'topic_prefix': '/' + uav_name},
+                    {'use_sim_time': use_sim_time},
+                    {'configs': configs},
+                    {'custom_config': custom_config},
+                    {'simulation': simulation},
 
                 ],
 
                 remappings=[
 
-                  ("~/ground_truth_in", "ground_truth" if simulation else "rtk/bestpos"),
-                  ("~/mavros_state_in", "mavros/state"),
-                  ("~/mavros_local_position_in", "mavros/local_position/odom"),
-                  ("~/mavros_odometry_in", mavros_odometry_in_topic),
-                  ("~/mavros_global_position_in", "mavros/global_position/global"),
-                  ("~/mavros_garmin_in", "mavros/garmin"),
-                  ("~/mavros_imu_in", "mavros/imu/data"),
-                  ("~/mavros_magnetometer_in", "mavros/global_position/compass_hdg"),
-                  ("~/mavros_magnetic_field_in", "mavros/imu/mag"),
-                  ("~/mavros_rc_in", "mavros/rc/in"),
-                  ("~/mavros_altitude_in", "mavros/altitude"),
-                  ("~/mavros_battery_in", "mavros/battery"),
-                  ("~/mavros_gps_status_raw_in", "mavros/gpsstatus/gps1/raw"),
+                  ('~/ground_truth_in', 'ground_truth' if simulation else 'rtk/bestpos'),
+                  ('~/mavros_state_in', 'mavros/state'),
+                  ('~/mavros_local_position_in', 'mavros/local_position/odom'),
+                  ('~/mavros_odometry_in', mavros_odometry_in_topic),
+                  ('~/mavros_global_position_in', 'mavros/global_position/global'),
+                  ('~/mavros_garmin_in', 'mavros/garmin'),
+                  ('~/mavros_imu_in', 'mavros/imu/data'),
+                  ('~/mavros_magnetometer_in', 'mavros/global_position/compass_hdg'),
+                  ('~/mavros_magnetic_field_in', 'mavros/imu/mag'),
+                  ('~/mavros_rc_in', 'mavros/rc/in'),
+                  ('~/mavros_altitude_in', 'mavros/altitude'),
+                  ('~/mavros_battery_in', 'mavros/battery'),
+                  ('~/mavros_gps_status_raw_in', 'mavros/gpsstatus/gps1/raw'),
 
-                  ("~/mavros_cmd_out", "mavros/cmd/command"),
-                  ("~/mavros_set_mode_out", "mavros/set_mode"),
-                  ("~/mavros_attitude_setpoint_out", "mavros/setpoint_raw/attitude"),
-                  ("~/mavros_actuator_control_out", "mavros/actuator_control"),
-                  ("~/errors", "errors"),
+                  ('~/mavros_cmd_out', 'mavros/cmd/command'),
+                  ('~/mavros_set_mode_out', 'mavros/set_mode'),
+                  ('~/mavros_attitude_setpoint_out', 'mavros/setpoint_raw/attitude'),
+                  ('~/mavros_actuator_control_out', 'mavros/actuator_control'),
+                  ('~/errors', 'errors'),
 
                 ],
             )
@@ -163,11 +163,9 @@ def generate_launch_description():
             namespace='',
             executable='static_transform_publisher',
             name='fcu_to_garmin',
-            arguments=["0.0", "0.0625", "-0.009", "0", "1.5708", "-1.5708", uav_name + "/fcu", uav_name + "/garmin"],
+            arguments=['0.0', '0.0625', '-0.009', '0', '1.5708', '-1.5708', uav_name + '/fcu', uav_name + '/garmin'],
             condition=IfCondition(simulation)
         )
     )
 
     return ld
-
-
--- launch/mavros_realworld.launch.py
+++ launch/mavros_realworld.launch.py
@@ -2,17 +2,9 @@
 
 import launch
 import os
-import launch_ros
 
-from launch_ros.actions import Node, PushROSNamespace, SetParameter
-from launch.actions import GroupAction, IncludeLaunchDescription, DeclareLaunchArgument
-from launch.substitutions import (
-    LaunchConfiguration,
-    IfElseSubstitution,
-    PythonExpression,
-    PathJoinSubstitution,
-    EnvironmentVariable,
-)
+from launch_ros.actions import Node, PushROSNamespace
+from launch.actions import GroupAction, IncludeLaunchDescription
 from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 
 from ament_index_python.packages import get_package_share_directory
@@ -23,46 +15,47 @@ def generate_launch_description():
 
     ld = launch.LaunchDescription()
 
-    pkg_name = "mrs_uav_px4_api"
+    pkg_name = 'mrs_uav_px4_api'
 
     this_pkg_path = get_package_share_directory(pkg_name)
 
     # #{ args from ENV
 
-    uav_name = os.getenv("UAV_NAME", "uav")
-    OLD_PX4_FW = os.getenv("OLD_PX4_FW", "false") == "true"
+    uav_name = os.getenv('UAV_NAME', 'uav')
+    OLD_PX4_FW = os.getenv('OLD_PX4_FW', 'false') == 'true'
+    PX4_IP = os.getenv('PX4_IP', '')
 
-    if OLD_PX4_FW:
-      fcu_url = "/dev/pixhawk:921600"
+    if PX4_IP:
+      fcu_url = f'udp://:14550@{PX4_IP}:14550'
     else:
-      fcu_url = "/dev/pixhawk:2000000"
+      rate = 921600 if OLD_PX4_FW else 2000000
+      fcu_url = f'/dev/pixhawk:{rate}'
 
-    gcs_url = "tcp-l://"
-    use_sim_time=os.getenv('USE_SIM_TIME', "false") == "true"
-    respawn_mavros=os.getenv('respawn_mavros', "false") == "true"
+    gcs_url = 'tcp-l://'
+    respawn_mavros = os.getenv('respawn_mavros', 'false') == 'true'
 
     # #} end of args from ENV
 
     # the first one has the priority
     # configs = [
     #     this_pkg_path + '/config/mavros_px4_config.yaml',
-    #     get_package_share_directory("mavros") + "/launch/px4_config.yaml",
+    #     get_package_share_directory('mavros') + '/launch/px4_config.yaml',
     # ]
 
     tgt_system = 1
     namespace = uav_name
 
     px4_launch_arguments = {
-        "fcu_url": fcu_url,
-        "gcs_url": gcs_url,
-        "tgt_system": str(tgt_system),
-        "tgt_component": str(1),
-        "log_output": "screen",
-        "fcu_protocol": "v2.0",
-        "respawn_mavros": str(respawn_mavros),
-        "namespace": "mavros",
-        "pluginlists_yaml":  this_pkg_path + "/config/mavros_plugins.yaml",
-        "config_yaml": this_pkg_path + "/config/mavros_px4_config_old_fw.yaml" if OLD_PX4_FW else this_pkg_path + "/config/mavros_px4_config.yaml",
+        'fcu_url': fcu_url,
+        'gcs_url': gcs_url,
+        'tgt_system': str(tgt_system),
+        'tgt_component': str(1),
+        'log_output': 'screen',
+        'fcu_protocol': 'v2.0',
+        'respawn_mavros': str(respawn_mavros),
+        'namespace': 'mavros',
+        'pluginlists_yaml': this_pkg_path + '/config/mavros_plugins.yaml',
+        'config_yaml': this_pkg_path + f'/config/mavros_px4_config{("_old_fw" if OLD_PX4_FW else "")}.yaml',
     }
 
     print(px4_launch_arguments.items())
@@ -70,7 +63,7 @@ def generate_launch_description():
     launch_xml_include_with_namespace = GroupAction(
         actions=[
             # push_ros_namespace first to set namespace of included nodes for following actions
-            PushROSNamespace(uav_name),
+            PushROSNamespace(namespace),
             IncludeLaunchDescription(
                 XMLLaunchDescriptionSource(
                     os.path.join(
@@ -81,19 +74,19 @@ def generate_launch_description():
             ),
         ],
         # parameters=[
-        #     {"config/garmin/frame_id": uav_name + "/garmin"},
+        #     {'config/garmin/frame_id': uav_name + '/garmin'},
         # ],
         # parameters=[
-        #     {"fcu_url": fcu_url},
-        #     {"gcs_url": gcs_url},
-        #     {"tgt_system": tgt_system},
-        #     {"tgt_component": 1},
-        #     {"log_output": "screen"},
-        #     {"fcu_protocol": "v2.0"},
-        #     {"respawn_mavros": respawn_mavros},
-        #     {"namespace": f"{namespace}/mavros"},
-        #     {"pluginlists_yaml": get_package_share_directory("mavros") + "/launch/px4_pluginlists.yaml"},
-        #     {"config_yaml": configs},
+        #     {'fcu_url': fcu_url},
+        #     {'gcs_url': gcs_url},
+        #     {'tgt_system': tgt_system},
+        #     {'tgt_component': 1},
+        #     {'log_output': 'screen'},
+        #     {'fcu_protocol': 'v2.0'},
+        #     {'respawn_mavros': respawn_mavros},
+        #     {'namespace': f'{namespace}/mavros'},
+        #     {'pluginlists_yaml': get_package_share_directory('mavros') + '/launch/px4_pluginlists.yaml'},
+        #     {'config_yaml': configs},
         # ],
     )
 
@@ -101,12 +94,12 @@ def generate_launch_description():
 
     ld.add_action(
         # Nodes under test
-        launch_ros.actions.Node(
+        Node(
             package='tf2_ros',
             namespace='',
             executable='static_transform_publisher',
             name='fcu_to_garmin',
-            arguments=["0.0", "0.0", "-0.05", "0", "1.57", "0", uav_name+"/fcu", "garmin"],
+            arguments=['0.0', '0.0', '-0.05', '0', '1.57', '0', uav_name + '/fcu', 'garmin'],
         )
     )
 
@@ -119,24 +112,22 @@ def generate_launch_description():
     #         executable='mavros_node',
 
     #         parameters=[
-    #             {"fcu_url": fcu_url},
-    #             {"gcs_url": gcs_url},
-    #             {"tgt_system": tgt_system},
-    #             {"tgt_component": 1},
-    #             {"log_output": "screen"},
-    #             {"fcu_protocol": "v2.0"},
-    #             {"respawn_mavros": respawn_mavros},
-    #             {"namespace": f"{namespace}/mavros"},
-    #             {"pluginlists_yaml": get_package_share_directory("mavros") + "/launch/px4_pluginlists.yaml"},
-    #             {"config_yaml": configs},
+    #             {'fcu_url': fcu_url},
+    #             {'gcs_url': gcs_url},
+    #             {'tgt_system': tgt_system},
+    #             {'tgt_component': 1},
+    #             {'log_output': 'screen'},
+    #             {'fcu_protocol': 'v2.0'},
+    #             {'respawn_mavros': respawn_mavros},
+    #             {'namespace': f'{namespace}/mavros'},
+    #             {'pluginlists_yaml': get_package_share_directory('mavros') + '/launch/px4_pluginlists.yaml'},
+    #             {'config_yaml': configs},
     #        ],
 
     #        remappings=[
-    #            ("/diagnostics", f"/uav{ID}/diagnostics")
+    #            ('/diagnostics', f'/uav{ID}/diagnostics')
     #         ],
     #     )
     # )
 
     return ld
-
-
